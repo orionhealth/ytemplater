9a9df7b2578fafd31c6cbf39e3293acfc28c334c
James Bunt <james.bunt@orionhealth.com>
2014-01-29 22:23:11 +1300

    Unit test precompiler.addReviveTemplateBoilerplate().

--- test/precompiler-addReviveTemplateBoilerplate.test.js
+++ test/precompiler-addReviveTemplateBoilerplate.test.js
@@ -1,15 +1,68 @@
 var expect = require('chai').expect,
 	stream = require('stream'),
+	Template = require('yui/template-base').Template,
 	PrecompilerTest = require('./precompiler-test');
 
 PrecompilerTest(function(testUtil) {
 	var precompiler = testUtil.precompiler;
 
 	describe('addReviveTemplateBoilerplate()', function() {
-		// TODO
+		var templates = [{
+				name: 'food'
+			}, {
+				name: 'name'
+			}],
+			boilerplate;
+
+		before(function() {
+			var stream = precompiler.addReviveTemplateBoilerplate(),
+				promise = testUtil.streamToPromise(stream);
+
+			templates.forEach(function(template) {
+				template.template = testUtil.getTestTemplate(template.name);
+				template.precompiled = new Template(testUtil.engine).precompile(template.template);
+			});
+
+			stream.write(templates[0]);
+			stream.end(templates[1]);
+
+			stream.end({
+				name: 'test-template',
+				template: testUtil.getTestTemplate('food')
+			});
+
+			return promise.then(function(data) {
+				// concat-stream should combine all the boilerplate into a single string
+				expect(data).to.be.a('string');
+
+				boilerplate = data;
+			});
+		});
 
 		it('should return a stream for wrapping precompiled templates in revive boilerplate code', function() {
 			expect(precompiler.addReviveTemplateBoilerplate()).to.be.an.instanceOf(stream);
+		});
+
+		it('should declare the templating engine once before the first template', function() {
+			var engineDeclaration = 'var engine = new Y.Template(' + testUtil.engineName + ');',
+				indexOf1stDeclaration,
+				indexOf2ndDeclaration;
+
+			expect(boilerplate).to.have.string(engineDeclaration);
+
+			indexOf1stDeclaration = boilerplate.indexOf(engineDeclaration);
+			expect(indexOf1stDeclaration).to.equal(0, 'expected engine declaration at start of boilerplate.');
+
+			indexOf2ndDeclaration = boilerplate.indexOf(engineDeclaration, engineDeclaration.length);
+			expect(indexOf2ndDeclaration).to.equal(-1, 'expected only one engine declaration.');
+		});
+
+		it('should wrap each template in boilerplate code to register and revive it', function() {
+			templates.forEach(function(template) {
+				expect(boilerplate).to.have.string(
+					'Y.Template.register(\'' + template.name + '\', engine.revive(' + template.precompiled + '));'
+				);
+			});
 		});
 
 	});
