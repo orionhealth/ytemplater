17586d3ff1600299b097f05c62c423532ca9f806
James Bunt <james.bunt@orionhealth.com>
2014-02-07 21:58:52 +1300

    A couple more additions to the stub CLI.

--- test/cli-help.test.js
+++ test/cli-help.test.js
@@ -4,17 +4,18 @@ var expect = require('chai').expect,
 	exec = require('child_process').exec;
 
 describe('ytemplater CLI', function() {
-	function expectUsageToBePrinted(err, stdout, stderr) {
-		expect(err).to.be.null;
-		expect(stderr).to.be.empty;
-
-		expect(stdout).to.contain('Usage: node ./bin/ytemplater [options] files...');
-		expect(stdout).to.contain('-h, --help  Show this usage information');
+	function expectUsageString(usage) {
+		expect(usage).to.contain('Usage: node ./bin/ytemplater [options] files...');
+		expect(usage).to.contain('-o, --out   File or directory to write the precompiled templates to');
+		expect(usage).to.contain('-h, --help  Show this usage information');
 	}
 
 	it('should print usage information on -h', function(done) {
 		exec('./bin/ytemplater -h', function(err, stdout, stderr) {
-			expectUsageToBePrinted(err, stdout, stderr);
+			expect(err).to.be.null;
+			expect(stderr).to.be.empty;
+
+			expectUsageString(stdout);
 
 			done();
 		});
@@ -22,7 +23,23 @@ describe('ytemplater CLI', function() {
 
 	it('should print usage information on --help', function(done) {
 		exec('./bin/ytemplater --help', function(err, stdout, stderr) {
-			expectUsageToBePrinted(err, stdout, stderr);
+			expect(err).to.be.null;
+			expect(stderr).to.be.empty;
+
+			expectUsageString(stdout);
+
+			done();
+		});
+	});
+
+	it('should log an error and print usage information to stderr if no files are provided', function(done) {
+		exec('./bin/ytemplater', function(err, stdout, stderr) {
+			expect(stdout).to.be.empty;
+
+			expect(err).to.exist;
+
+			expectUsageString(stderr);
+			expect(stderr).to.contain('At least one file is required.');
 
 			done();
 		});
